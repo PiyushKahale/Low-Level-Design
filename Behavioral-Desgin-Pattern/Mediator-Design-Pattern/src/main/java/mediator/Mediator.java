8b680d3cf020f597ae29be6ba2216f17d4964442
Piyush Kahale <93856290+PiyushKahale@users.noreply.github.com>
2026-03-22 10:49:22 +0530

    added few diagrams

---
+++ Behavioral-Desgin-Pattern/Mediator-Design-Pattern/src/main/java/mediator/Mediator.java
@@ -0,0 +1,10 @@
+package mediator;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Mediator {
+    public static void main(String[] args) {
+
+
+    }
+}
